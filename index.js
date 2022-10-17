7f8f91c5bf824912d146ccb98482e1aa28ac45d2
ellis0n <ellisonglenmay@gmail.com>
2022-10-16 21:43:26 -0230

    express server implemenet

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require('./middleware/logEvents')
 const PORT = process.env.PORT || 3500;
+const cors = require("cors")
+const errorHandler = require('./middleware/errorHandler')
+
+//custom middleware logger
+app.use(logger)
+//Cross-origin resource sharing
+const whitelist = [ 
+  'https://www.twack.com',
+  'http://127.0.0.1:5500',
+  'http://localhost:3500',
+  'http://localhost:3500/'
+]
+const corsOptions = {
+  origin: (origin, callback)=>{
+    if (whitelist.indexOf(origin)  !== -1 || !origin){ //TODO delete !origin
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 
 // BUILT-IN FORMDATA:
 app.use(express.urlencoded({ extended: false }));
+
 // BUILT-IN JSON DATA:
 app.use(express.json());
+
 // SERVE STATIC FILES
-app.use(express.static(path.join(__dirname, "/public")));
+app.use('/', express.static(path.join(__dirname, "/public")));
+app.use('/subdir', express.static(path.join(__dirname, "/public")));
 
-// Serve a HTML file
-app.get("^/$|/index(.html)?", (req, res) => {
-  //Regex
-  //res.sendFile("./views/index.html", { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+app.use('/', require('./routes/root'));
+app.use('/subdir',require('./routes/subdir'));
+app.use('/users',require('./routes/api/users'));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts('json')) {
+    res.json({error: "404 not found"})
+  } else { 
+  res.type('txt').send("404 Not Found")
+};
 });
 
-app.get("/*", (req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-});
 
-// Route handlers:
-app.get(
-  "/hello(.html)?",
-  (req, res, next) => {
-    console.log("Attempted to load hello.html");
-    next();
-  },
-  (req, res) => {
-    "hello world";
-  }
-);
+app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- scrape.js
+++ scrape.js
@@ -1,27 +1,16 @@
 const params = {
-  locationId: 1700185, // Same as kijiji.locations.ONTARIO.OTTAWA_GATINEAU_AREA.OTTAWA
-  categoryId: 27, // Same as kijiji.categories.CARS_AND_VEHICLES
-  sortByName: "priceAsc", // Show the cheapest listings first
+  locationId: 1700185, 
+  categoryId: 27, 
+  sortByName: "priceAsc", 
 };
 
+let adArray = [];
 // Scrape using returned promise
 kijiji
   .search(params, options)
   .then((ads) => {
-    // Use the ads array
     for (let i = 0; i < ads.length; ++i) {
       console.log(ads[i].title);
     }
   })
   .catch(console.error);
-
-// Scrape using optional callback parameter
-function callback(err, ads) {
-  if (!err) {
-    // Use the ads array
-    for (let i = 0; i < ads.length; ++i) {
-      console.log(ads[i].title);
-    }
-  }
-}
-kijiji.search(params, options, callback);
