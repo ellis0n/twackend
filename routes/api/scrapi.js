7449bd94b22a94482382eaf5f55125072b300703
ellis0n <ellisonglenmay@gmail.com>
2022-11-07 08:07:15 -0330

    changes to voting req

--- routes/api/scrapi.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const adController = require("../../controllers/adController");
-
-router.post("/ads", adController.scrapeAds);
-
-module.exports = router;
