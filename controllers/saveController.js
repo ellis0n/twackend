8b888bfd0df9f5bfb1235c74afeec326fb4a9741
glen may <ellisonglenmay@gmail.com>
2022-11-24 16:29:37 -0330

    implemented save ad data to DB

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -1,13 +1,11 @@
 const Save = require("../model/Save");
 
 const saveAd = async (req, res) => {
-  console.log(req.body);
   try {
     const result = await Save.create({
-      id: req.body.id,
+      ad: JSON.parse(req.body.ad),
       vote: req.body.vote,
     });
-    console.log(result);
     res.status(201).json(result);
   } catch (err) {
     console.error(err);
@@ -20,7 +18,6 @@ const getAllSavedAds = async (req, res) => {
     return res.status(204).json({ message: "No saved ads found." });
   res.json(savedAds);
 
-  console.log(req.body);
   const saveFile = path.join(__dirname, "..", "model", "ads.json");
   fs.readFile(saveFile, "utf-8", (error, data) => {
     if (error) throw error;
