4630e7d62ab4ad573d1ff8622dd6aa4724571cc1
glen may <ellisonglenmay@gmail.com>
2022-11-25 17:22:06 -0330

    post (broken)

--- controllers/saveController.js
+++ controllers/saveController.js
@@ -21,21 +21,22 @@ const getAllSavedAds = async (req, res) => {
 
 const updateVote = async (req, res) => {
   try {
-    const ad = await Save.findOne({ _id: req.body.id }.exec());
+    const ad = await Save.findOne({ id: req.body }).exec();
     if (!ad) {
       return res.status(204).json({ message: `No ad matches ${req.body.id}` });
     }
+    ad.vote = req.body;
     const result = await ad.save();
-    res.json(result);
+    res.json(ad);
   } catch (err) {
     console.error(err);
   }
 };
 
 const deleteVote = async (req, res) => {
-  console.log(req.body);
+  let deleteAd = await Save.find({ id: req.body });
   const result = await Save.deleteOne({ id: req.body });
-  res.status(201).json(result);
+  res.status(200).json(result);
 };
 
 // const result = await Save.find({_id});
