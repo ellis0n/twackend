49015b25ff177f151ca3e8b98f2bef385c465f08
glen may <ellisonglenmay@gmail.com>
2022-12-05 16:52:16 -0330

    return only unviewed ads

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -1,11 +1,11 @@
 const kijiji = require("kijiji-scraper");
+const Save = require("../model/Save");
 
 //  Core scraper functionality
 //  TODO:: check for user saved ads, dont return ad already voted
 
 const scrape = async (parameters) => {
   let adArray = [];
-console.log(parameters)
   try {
     const params = {
       locationId: JSON.parse(parameters.location),
@@ -22,21 +22,47 @@ console.log(parameters)
           price: ad.attributes.price,
           url: ad.url,
           desc: ad.description,
-          status: ad.isScraped,
         };
-        adArray.push(newAdObj);
+
+        if (
+          Object.values(newAdObj).every(
+            (value) => value !== "" && value !== undefined
+          )
+        ) {
+          adArray.push(newAdObj);
+        }
       }
+      // console.log("HERE ARE ADS SCRAPED:");
+      // console.log(adArray);
     });
   } catch (err) {
     throw err;
   }
-  return adArray;
+  let filteredAds = await filterAds(adArray);
+  return filteredAds;
+};
+
+const filterAds = async (adArray) => {
+  let newAdArray = [];
+  for (let i = 0; i < adArray.length; i++) {
+    let ad = adArray[i];
+    let search = await Save.findOne({ ad: ad }).exec();
+    if (!search) {
+      newAdArray.push(ad);
+    }
+  }
+  return newAdArray;
 };
 
 const scrapeAds = async (req, res) => {
   console.log(req.body);
   const ads = await scrape(req.body);
   const jsonAds = JSON.stringify(ads);
+  if (!jsonAds) {
+    return res
+      .status(200)
+      .json({ message: "No more ads to view. Try again later!" });
+  }
   return res.status(200).json(jsonAds);
 };
 
