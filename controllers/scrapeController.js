1676ef6f6b6b434fc345b5b084f9f4faa7a9d351
glen may <ellisonglenmay@gmail.com>
2022-12-05 23:38:06 -0330

    simplifying the filter process

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -18,52 +18,43 @@ const scrape = async (parameters) => {
       minResults: 20,
     };
 
-    await kijiji.search(params, options).then((ads) => {
-      // console.log(ads.length);
+    const ads = await kijiji.search(params, options).then((scrapedAds) => { return scrapedAds });
+    // console.log(ads.length)
 
-      for (let i = 0; i < ads.length; i++) {
-        let ad = ads[i];
+    for (let i = 0; i < ads.length; i++) {
+      let ad = ads[i];
+      console.log(ad)
+      let search = await Save.findOne({ 'ad.id': ad.id }).exec();
+      if (!search) {{
+        if (ad.image &&
+            ad.attributes.price && 
+            ad.description){
 
-        newAdObj = {
-          id: ad.id,
-          img: ad.image,
-          title: ad.title.toUpperCase(),
-          price: ad.attributes.price,
-          url: ad.url,
-          desc: ad.description,
-        };
-
-        //  find a more efficient way to filter before creating newAdObj
-        if (
-          Object.values(newAdObj).every(
-            (value) => value !== "" && value !== undefined
-          )
-        ) {
+              newAdObj = {
+              id: ad.id,
+              img: ad.image,
+              //images: ad.imagines 
+              title: ad.title.toUpperCase(),
+              price: ad.attributes.price,
+              url: ad.url,
+              desc: ad.description,
+              date: ad.date,
+              location: ad.attributes.location,
+              isScraped: ad.isScraped()
+            };
+          console.log(newAdObj)
           adArray.push(newAdObj);
+          }
         }
-        // console.log(adArray.length)
       }
-    });
+    }
+    // console.log(adArray.length)
   } catch (err) {
     throw err;
   }
-  // Do this at the same time as iterating
-  const filteredAds =  filterAds(adArray);
-  return filteredAds;
+  return adArray
 };
 
-const filterAds = async (adArray) => {
-  let newAdArray = [];
-  for (let i = 0; i < adArray.length; i++) {
-    let ad = adArray[i];
-    let search = await Save.findOne({ ad: ad }).exec();
-    if (!search) {
-      newAdArray.push(ad);
-    }
-  }
-  // console.log(newAdArray.length);
-  return newAdArray;
-};
 
 const scrapeAds = async (req, res) => {
   console.log(req.body);
