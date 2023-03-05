f4da7d561cf6eebb3c0b0fe00791570d9d7c9831
glen may <ellisonglenmay@gmail.com>
2023-03-04 22:10:03 -0330

    up to 100 results for easier testing

--- controllers/scrapeController.js
+++ controllers/scrapeController.js
@@ -5,64 +5,64 @@ const Vote = require("../model/Vote");
 
 // Kijiji scraper functionality
 const scrapeAds = async (req, res) => {
-  const ads = await scrape(req.body);
-  const jsonAds = JSON.stringify(ads);
-  return res.status(200).json(jsonAds);
+	const ads = await scrape(req.body);
+	const jsonAds = JSON.stringify(ads);
+	return res.status(200).json(jsonAds);
 };
 
 const scrape = async ({ params, user }) => {
-  let adArray = [];
-  try {
-    const parameters = {
-      locationId: JSON.parse(params.location),
-      categoryId: JSON.parse(params.category),
-      sortByName: "dateAsc",
-    };
+	let adArray = [];
+	try {
+		const parameters = {
+			locationId: JSON.parse(params.location),
+			categoryId: JSON.parse(params.category),
+			sortByName: "dateAsc",
+		};
 
-    const options = {
-      minResults: 20,
-    };
+		const options = {
+			minResults: 100,
+		};
 
-    const ads = await kijiji.search(parameters, options).then((scrapedAds) => {
-      return scrapedAds;
-    });
+		const ads = await kijiji.search(parameters, options).then((scrapedAds) => {
+			return scrapedAds;
+		});
 
-    const findVotes = await Vote.findOne({
-      username: user,
-    }).exec();
+		const findVotes = await Vote.findOne({
+			username: user,
+		}).exec();
 
-    for (let i = 0; i < ads.length; i++) {
-      let ad = ads[i];
+		for (let i = 0; i < ads.length; i++) {
+			let ad = ads[i];
 
-      let check = findVotes
-        ? findVotes.votes.for.includes(ad.id) ||
-          findVotes.votes.against.includes(ad.id)
-        : false;
+			let check = findVotes
+				? findVotes.votes.for.includes(ad.id) ||
+				  findVotes.votes.against.includes(ad.id)
+				: false;
 
-      if (!check) {
-        if (ad.image && ad.attributes.price && ad.description) {
-          newAdObj = {
-            id: ad.id,
-            img: ad.image,
-            //images: ad.images
-            title: ad.title.toUpperCase(),
-            price: ad.attributes.price,
-            url: ad.url,
-            desc: ad.description,
-            date: ad.date,
-            location: ad.attributes.location,
-            isScraped: ad.isScraped(),
-          };
-          adArray.push(newAdObj);
-        }
-      }
-    }
-  } catch (err) {
-    throw err;
-  }
-  return adArray;
+			if (!check) {
+				if (ad.image && ad.attributes.price && ad.description) {
+					newAdObj = {
+						id: ad.id,
+						img: ad.image,
+						images: ad.images,
+						title: ad.title.toUpperCase(),
+						price: ad.attributes.price,
+						url: ad.url,
+						desc: ad.description,
+						date: ad.date,
+						location: ad.attributes.location,
+						isScraped: ad.isScraped(),
+					};
+					adArray.push(newAdObj);
+				}
+			}
+		}
+	} catch (err) {
+		throw err;
+	}
+	return adArray;
 };
 
 module.exports = {
-  scrapeAds,
+	scrapeAds,
 };
