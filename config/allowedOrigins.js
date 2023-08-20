12e355ff3047f67b437ebc548525b54145309c30
ellis0n <100211236+ellis0n@users.noreply.github.com>
2023-08-20 17:36:35 -0230

    add  gh pages to allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
 	"http://localhost:3500",
 	"http://localhost:3000",
 	"https://twackend-b59mi.ondigitalocean.app/",
+	"https://ellis0n.github.io",
 ];
 
 module.exports = allowedOrigins;
