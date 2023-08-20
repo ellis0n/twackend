d6df238693524c89f80f4e37da9dc271cdbb1755
glen may <ellisonglenmay@gmail.com>
2023-08-20 15:12:37 -0230

    cors fix

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -4,7 +4,7 @@ const credentials = (req, res, next) => {
 	const origin = req.headers.origin;
 	if (allowedOrigins.includes(origin)) {
 		console.log("origin: ", origin);
-		// res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+		res.set("Access-Control-Allow-Origin", "http://localhost:3000");
 		res.set("Access-Control-Allow-Credentials", true);
 	}
 	next();
