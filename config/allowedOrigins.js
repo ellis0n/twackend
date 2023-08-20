993aadab1aa4eb11e9f8751fb280646c56bd6a15
glen may <ellisonglenmay@gmail.com>
2023-08-20 15:11:29 -0230

    staging a deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-  "https://www.twack.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3000",
+	"https://www.twack.com",
+	"http://127.0.0.1:5500",
+	"http://localhost:3500",
+	"http://localhost:3000",
+	"https://twackend-b59mi.ondigitalocean.app/",
 ];
 
-module.exports = allowedOrigins
+module.exports = allowedOrigins;
--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,13 +1,13 @@
 const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    console.log("origin: ", origin);
-    res.set("Access-Control-Allow-Origin", "http://localhost:3000"); 
-    res.set("Access-Control-Allow-Credentials", true);
-  }
-  next();
+	const origin = req.headers.origin;
+	if (allowedOrigins.includes(origin)) {
+		console.log("origin: ", origin);
+		// res.set("Access-Control-Allow-Origin", "http://localhost:3000");
+		res.set("Access-Control-Allow-Credentials", true);
+	}
+	next();
 };
 
 module.exports = credentials;
